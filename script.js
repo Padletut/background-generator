8ba8ba397e024227ca64edd3179b0e83a97736a7
unknown <padletut@gmail.com>
2021-11-08 20:34:18 +0100

    new files

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
 
-//Some array methods()
+	css.textContent = body.style.background + ";";
+}
 
-// list.shift(); Deletes first data in an Array
-// list.pop();  Deletes last data in an Array
-// list.push("elephant"); Add data to end of an Array. Needs argument.
-// let newArray = list.concat(["bee", "deer"]); Merge an Array with list Array to an new Array
-// list.sort(); Sorts Array.
+color1.addEventListener("input", setGradient);
 
-
-// using this array,
-// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
-
-
-// 1. Remove the Banana from the array.
-
-// 2. Sort the array in order.
-
-// 3. Put "Kiwi" at the end of the array.
-
-// 4. Remove "Apples" from the array.
-
-// 5. Sort the array in reverse order. (Not alphabetical, but reverse
-// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
-
-// you should have at the end:
-// ["Kiwi", "Oranges", "Blueberries"]
-
-// Remove Banana
-array.shift();
-
-// Sort Array
-array.sort();
-
-// Add "Kiwi" to the end of Array
-array.push("Kiwi");
-
-// Remove Apples
-array.shift();
-
-// Sort Array reverse
-
-array.reverse();
-
-// using this array,
-// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-// access "Oranges".
-
-let array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-array2[1][1][0]; // access "Oranges"
+color2.addEventListener("input", setGradient);
